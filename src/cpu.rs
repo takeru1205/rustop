351ae0616523f616bbfc6de02f1aa3eb4049032c
takeru1205 <takeru0639418@gmail.com>
2023-04-20 00:04:41 +0900

    Update display_cpu_info to show CPU usage bars in 2 columns

--- src/cpu.rs
+++ src/cpu.rs
@@ -14,36 +14,39 @@ pub fn display_cpu_info(
 ) -> Result<u16> {
     sys.refresh_cpu(); // Refreshing CPU information.
 
-    for cpu in sys.cpus().iter() {
-        // let cpu_usage: u16 = ((100. as f32) / 100. * ((width - crate::EDGE) as f32)) as u16;
-        let cpu_usage: u16 =
-            ((cpu.cpu_usage() as f32) / 100. * ((width - crate::EDGE) as f32)) as u16;
-        // Change color bar depends on usage
-        let print_style: style::PrintStyledContent<&str>;
-        if cpu_usage < 40 {
-            print_style = style::PrintStyledContent("█".green());
-        } else if cpu_usage < 80 {
-            print_style = style::PrintStyledContent("█".yellow());
-        } else {
-            print_style = style::PrintStyledContent("█".red());
-        }
+    let cpus = sys.cpus();
+    let half_width = (width - 3) / 2;
 
-        // Print usage bar
-        for x in crate::X..(crate::X + 100) {
-            if x <= cpu_usage {
-                queue!(stdout, cursor::MoveTo(x, *y), print_style).unwrap();
+    // Display CPU usage bars in 2 columns
+    for chunk in cpus.chunks(2) {
+        for (index, cpu) in chunk.iter().enumerate() {
+            let cpu_usage: u16 = ((cpu.cpu_usage() as f32) / 100.0 * (half_width as f32)) as u16;
+
+            let print_style: style::PrintStyledContent<&str>;
+            if cpu_usage < 40 {
+                print_style = style::PrintStyledContent("█".green());
+            } else if cpu_usage < 80 {
+                print_style = style::PrintStyledContent("█".yellow());
             } else {
+                print_style = style::PrintStyledContent("█".red());
+            }
+
+            let x_offset = 3 + index as u16 * half_width;
+            for x in x_offset..(x_offset + cpu_usage) {
+                queue!(stdout, cursor::MoveTo(x, *y), print_style).unwrap();
+            }
+
+            for x in (x_offset + cpu_usage)..(x_offset + half_width) {
                 queue!(stdout, cursor::MoveTo(x, *y), Print(" ")).unwrap();
             }
-        }
 
-        // Debug to print cpu usage
-        queue!(
-            stdout,
-            cursor::MoveTo(3, *y),
-            Print(format!("{: >02}", cpu_usage))
-        )
-        .unwrap();
+            queue!(
+                stdout,
+                cursor::MoveTo(3 + index as u16 * half_width, *y),
+                Print(format!("{: >02}", cpu_usage))
+            )
+            .unwrap();
+        }
 
         *y += 1;
     }
