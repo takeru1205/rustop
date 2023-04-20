6e68eb58262577dd2627aaf9f00eb5532a4a5e95
takeru1205 <takeru0639418@gmail.com>
2023-04-20 21:38:07 +0900

    [clean] delete const width and height

--- src/frame.rs
+++ src/frame.rs
@@ -1,14 +1,15 @@
 use crossterm::{
     cursor, queue,
     style::{self, Stylize},
-    Result,
+    terminal, Result,
 };
 use std::io::Write;
 
 pub fn draw_frame(stdout: &mut impl Write) -> Result<()> {
-    for y in 0..crate::HEIGHT {
-        for x in 0..crate::WIDTH {
-            if (y == 0 || y == crate::HEIGHT - 1) || (x == 0 || x == crate::WIDTH - 1) {
+    let (width, height) = terminal::size().unwrap();
+    for y in 0..height {
+        for x in 0..width {
+            if (y == 0 || y == height - 1) || (x == 0 || x == width - 1) {
                 queue!(
                     stdout,
                     cursor::MoveTo(x, y),
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,7 @@ mod frame;
 mod memory;
 mod nvidia;
 
-pub const WIDTH: u16 = 150; // Width of window
-pub const HEIGHT: u16 = 40; // Height of window
-pub const EDGE: u16 = 10; // Edge of frame
+pub const EDGE: u16 = 3; // Edge of frame
 pub const X: u16 = 10; // Left end line
 pub const Y_INIT: u16 = 1; // Start line
 const REFRESH: u64 = 500; // Frequency to refresh information
@@ -24,11 +22,9 @@ fn main() -> Result<()> {
     let mut stdout = stdout();
     execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
 
-    let (width, _) = terminal::size().unwrap(); // get Terminal size
-
     for _ in 0..10 {
         let mut y: u16 = Y_INIT;
-        y = cpu::display_cpu_info(&mut sys, &mut stdout, &mut y, width)?;
+        y = cpu::display_cpu_info(&mut sys, &mut stdout, &mut y)?;
         y = memory::display_memory_info(&mut sys, &mut stdout, &mut y)?;
         _ = nvidia::display_gpu_info(&device, &mut stdout, &mut y)?;
         frame::draw_frame(&mut stdout)?;
