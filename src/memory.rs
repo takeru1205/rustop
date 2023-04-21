472d460abae8a25460fba717a290ead3c064d236
takeru1205 <takeru0639418@gmail.com>
2023-04-22 00:51:53 +0900

    [clean] remove debug print

--- src/memory.rs
+++ src/memory.rs
@@ -8,19 +8,6 @@ pub fn display_memory_info(sys: &mut System, stdout: &mut impl Write, y: &mut u1
     sys.refresh_memory(); // Refreshing memory information.
     let (width, _) = terminal::size().unwrap();
     let half_width = (width - crate::EDGE) / 2;
-    let memory_usage: Vec<u64> = vec![
-        sys.total_memory(),
-        sys.used_memory(),
-        sys.total_swap(),
-        sys.used_swap(),
-    ];
-
-    let memory_use_case: Vec<String> = vec![
-        "total memory".to_string(),
-        "used memory".to_string(),
-        "total swap".to_string(),
-        "used swap".to_string(),
-    ];
 
     // Display memory usage info
     let used_mem_mb = mem_to_mb(sys.used_memory());
@@ -68,22 +55,6 @@ pub fn display_memory_info(sys: &mut System, stdout: &mut impl Write, y: &mut u1
     .unwrap();
     *y += 1;
 
-    // Display infos
-    for (case, usage) in memory_use_case.iter().zip(memory_usage.iter()) {
-        queue!(
-            stdout,
-            cursor::MoveTo(crate::X, *y),
-            Print(format!("{}: {} bytes", case, usage))
-        )?;
-        *y += 1;
-    }
-
-    queue!(
-        stdout,
-        cursor::MoveTo(crate::X, *y),
-        Print(format!("{}", memory_usage_percentage))
-    )?;
-    *y += 1;
     Ok(*y)
 }
 
