4dee866e7cda3a692900ce04ac18d48cc0880371
takeru1205 <takeru0639418@gmail.com>
2023-04-22 00:23:23 +0900

    [update] add usage bar

--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,5 @@
+use crate::bar;
+use crate::bar::PreID;
 use crossterm::{cursor, queue, style::Print, Result};
 use std::io::Write;
 use sysinfo::{System, SystemExt};
@@ -18,6 +20,27 @@ pub fn display_memory_info(sys: &mut System, stdout: &mut impl Write, y: &mut u1
         "used swap".to_string(),
     ];
 
+    let memory_usage_percentage =
+        mem_to_gb(sys.used_memory()) / mem_to_gb(sys.total_memory()) * 100.;
+    let swap_usage_percentage = mem_to_gb(sys.used_swap()) / mem_to_gb(sys.total_swap()) * 100.;
+    _ = &mut bar::display_usage_bar(
+        memory_usage_percentage as f32,
+        0,
+        stdout,
+        y,
+        PreID::DispName("RAM".to_string()),
+    )
+    .unwrap();
+    _ = &mut bar::display_usage_bar(
+        swap_usage_percentage as f32,
+        1,
+        stdout,
+        y,
+        PreID::DispName("Swap".to_string()),
+    )
+    .unwrap();
+    *y += 1;
+
     for (case, usage) in memory_use_case.iter().zip(memory_usage.iter()) {
         queue!(
             stdout,
@@ -26,5 +49,16 @@ pub fn display_memory_info(sys: &mut System, stdout: &mut impl Write, y: &mut u1
         )?;
         *y += 1;
     }
+
+    queue!(
+        stdout,
+        cursor::MoveTo(crate::X, *y),
+        Print(format!("{}", memory_usage_percentage))
+    )?;
+    *y += 1;
     Ok(*y)
+}
+
+fn mem_to_gb(mem: u64) -> f64 {
+    (mem * 1024) as f64 / (1024. * 1024. * 1024.)
 }
