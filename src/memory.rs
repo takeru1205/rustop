2843690688f50b9ed0eb0100bfbea27475617089
takeru1205 <takeru0639418@gmail.com>
2023-04-22 00:44:13 +0900

    [update] display bar and usage

--- src/memory.rs
+++ src/memory.rs
@@ -1,11 +1,13 @@
 use crate::bar;
 use crate::bar::PreID;
-use crossterm::{cursor, queue, style::Print, Result};
+use crossterm::{cursor, queue, style::Print, terminal, Result};
 use std::io::Write;
 use sysinfo::{System, SystemExt};
 
 pub fn display_memory_info(sys: &mut System, stdout: &mut impl Write, y: &mut u16) -> Result<u16> {
     sys.refresh_memory(); // Refreshing memory information.
+    let (width, _) = terminal::size().unwrap();
+    let half_width = (width - crate::EDGE) / 2;
     let memory_usage: Vec<u64> = vec![
         sys.total_memory(),
         sys.used_memory(),
@@ -20,9 +22,10 @@ pub fn display_memory_info(sys: &mut System, stdout: &mut impl Write, y: &mut u1
         "used swap".to_string(),
     ];
 
-    let memory_usage_percentage =
-        mem_to_gb(sys.used_memory()) / mem_to_gb(sys.total_memory()) * 100.;
-    let swap_usage_percentage = mem_to_gb(sys.used_swap()) / mem_to_gb(sys.total_swap()) * 100.;
+    // Display memory usage info
+    let used_mem_mb = mem_to_mb(sys.used_memory());
+    let total_mem_mb = mem_to_mb(sys.total_memory());
+    let memory_usage_percentage = used_mem_mb / total_mem_mb * 100.;
     _ = &mut bar::display_usage_bar(
         memory_usage_percentage as f32,
         0,
@@ -31,16 +34,41 @@ pub fn display_memory_info(sys: &mut System, stdout: &mut impl Write, y: &mut u1
         PreID::DispName("RAM".to_string()),
     )
     .unwrap();
+    queue!(
+        stdout,
+        cursor::MoveTo(crate::EDGE + half_width, *y),
+        Print(format!(
+            "{0: >10} MB / {1: >10} MB",
+            used_mem_mb, total_mem_mb
+        ))
+    )
+    .unwrap();
+    *y += 1;
+
+    // Display Swap usage info
+    let used_swap_mb = mem_to_mb(sys.used_swap());
+    let total_swap_mb = mem_to_mb(sys.total_swap());
+    let swap_usage_percentage = used_swap_mb / total_swap_mb * 100.;
     _ = &mut bar::display_usage_bar(
         swap_usage_percentage as f32,
-        1,
+        0,
         stdout,
         y,
         PreID::DispName("Swap".to_string()),
     )
     .unwrap();
+    queue!(
+        stdout,
+        cursor::MoveTo(crate::EDGE + half_width, *y),
+        Print(format!(
+            "{0: >10} MB / {1: >10} MB",
+            used_swap_mb, total_swap_mb
+        ))
+    )
+    .unwrap();
     *y += 1;
 
+    // Display infos
     for (case, usage) in memory_use_case.iter().zip(memory_usage.iter()) {
         queue!(
             stdout,
@@ -59,6 +87,6 @@ pub fn display_memory_info(sys: &mut System, stdout: &mut impl Write, y: &mut u1
     Ok(*y)
 }
 
-fn mem_to_gb(mem: u64) -> f64 {
-    (mem * 1024) as f64 / (1024. * 1024. * 1024.)
+fn mem_to_mb(mem: u64) -> f64 {
+    (mem * 1024) as f64 / (1024. * 1024.)
 }
