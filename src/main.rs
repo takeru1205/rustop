b4d457fcc38e6e9df399cda7678719fd2c5813b2
takeru1205 <takeru0639418@gmail.com>
2023-04-23 00:30:28 +0900

    [update] infinite loop until to press q key

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use crossterm::{cursor, execute, queue, style::Print, terminal, Result};
-// use crossterm::{execute, terminal, Result};
+use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode},
+    execute, queue,
+    style::Print,
+    terminal, Result,
+};
 use nvml_wrapper::Nvml;
 use std::io::{stdout, Write};
 use sysinfo::{System, SystemExt};
@@ -11,11 +16,11 @@ mod model;
 mod nvidia;
 mod pie_chart;
 
-pub const EDGE: u16 = 4; // Edge of frame
-pub const RADIUS: u16 = 10; // Radius of pie chart
-pub const X: u16 = 10; // Left end line
-pub const Y_INIT: u16 = 1; // Start line
-const REFRESH: u64 = 500; // Frequency to refresh information
+pub const EDGE: u16 = 4;
+pub const RADIUS: u16 = 10;
+pub const X: u16 = 10;
+pub const Y_INIT: u16 = 1;
+const REFRESH: u64 = 500;
 
 fn main() -> Result<()> {
     let mut sys = System::new();
@@ -23,12 +28,11 @@ fn main() -> Result<()> {
     let device = nvml.device_by_index(0).unwrap();
     let cpu_model = model::get_cpu_model().unwrap();
 
-    // Refresh the default terminal
     let mut stdout = stdout();
     execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
 
-    for _ in 0..10 {
-        // Display the CPU model
+    terminal::enable_raw_mode()?;
+    loop {
         queue!(
             stdout,
             cursor::MoveTo(crate::X, Y_INIT),
@@ -43,13 +47,25 @@ fn main() -> Result<()> {
         _ = nvidia::display_gpu_info(&device, &mut stdout, &mut mem_y)?;
         frame::draw_frame(&mut stdout)?;
 
-        // Refresh the terminal
         stdout.flush().unwrap();
 
-        // Sleep between updates
-        std::thread::sleep(std::time::Duration::from_millis(REFRESH));
+        if event::poll(std::time::Duration::from_millis(REFRESH))? {
+            let evt = event::read()?;
+
+            if let Event::Key(key) = evt {
+                if key.code == KeyCode::Char('q') {
+                    queue!(
+                        stdout,
+                        terminal::Clear(terminal::ClearType::All),
+                        cursor::MoveTo(0, 0)
+                    )?;
+                    break;
+                }
+            }
+        }
     }
+
     stdout.flush()?;
-    println!("");
+    terminal::disable_raw_mode()?;
     Ok(())
 }
